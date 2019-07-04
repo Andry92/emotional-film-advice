646266e2433eda551b62e5d0280a1582a8ec1e8b
andrea <andreadibenedetto92@gmail.com>
2019-07-04 15:40:12 +0200

    various fix in cosine_similarity and add movie_dataset_very_small.csv

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -11,35 +11,51 @@ tweet_topic = []
 with open('tweet_topics.csv', 'r+') as tweet_file:
     for tweet in tweet_file.readlines():
         tweet_topic.append(tweet.replace("\n", ""))
-        with open('movie_dataset.csv', 'r+') as movie_file:
-            reader = csv.reader(movie_file, delimiter=';')
-            for row in reader:
-                movie_topic.append(row[2])
 
-#print("".join(tweet_topic))
-list_all = []
+with open('movie_dataset_very_small.csv', 'r+') as movie_file:
+    reader = csv.reader(movie_file, delimiter=';')
+    for row in reader:
+        movie_topic.append(row[2])
 
-for i in range(0,len(tweet_topic)):
-    print(i)
-    print(tweet_topic[i])
-    documents = tweet_topic[i], movie_topic
+print(len(tweet_topic))
+print(len(movie_topic))
 
-    documents = [tweet_topic[i]]
+num_tweet_topic = len(tweet_topic)
+for i in range(1, num_tweet_topic+1):
+    documents = []
+    documents = [tweet_topic[i-1]]
+
     for elem in movie_topic:
         documents.append(elem)
+        #print("ELEM >  ",elem)
 
-    #pprint(documents)
+    #print("A>", documents)
+
+
+    pprint(documents)
     count_vectorizer = CountVectorizer()
     sparse_matrix = count_vectorizer.fit_transform(documents)
-    pprint(sparse_matrix)
+    #print("SPARSE MATRIX\n", sparse_matrix)
     doc_term_matrix = sparse_matrix.todense()
-    df = pd.DataFrame(doc_term_matrix,
-                      columns=count_vectorizer.get_feature_names(),
-                      index=[])
-pprint(df)
+    #print("DOC_TERM_MATRIX\n", doc_term_matrix)
+    df = pd.DataFrame(doc_term_matrix, columns=count_vectorizer.get_feature_names())
 
-# Define the documents
+    pprint(df)
+    cos_sim = cosine_similarity(df, df)
+    #print(cos_sim)
+
+    first_column = cos_sim[:, 0]
+    #print(first_column)
+    sort_first_column = sorted(first_column)
+    #print(sort_first_column[-2])
+    indice_topic_trama = np.where(sort_first_column == sort_first_column[-2])
+    print("\n\nINDICE TOPIC TRAMA:", indice_topic_trama[0])
+
 '''
+# print("\n",np.amax(cos_sim))
+ # print("\n",cos_sim.max(axis=0)[0])
+# Define the documents
+
 doc_trump = "Mr. Trump became president after winning the political election. Though he lost the support of some republican friends, Trump is friends with President Putin"
 
 doc_election = "President Trump says Putin had no political interference is the election outcome. He says it was a witchhunt by political parties. He claimed President Putin is a friend who had nothing to do with the election"
@@ -47,9 +63,9 @@ doc_election = "President Trump says Putin had no political interference is the 
 doc_putin = "Post elections, Vladimir Putin became President of Russia. President Putin had served as the Prime Minister earlier in his political career"
 
 documents = [doc_trump, doc_election, doc_putin]
-'''
 
-'''
+
+
 # Create the Document Term Matrix
 count_vectorizer = CountVectorizer(stop_words='english')
 count_vectorizer = CountVectorizer()
