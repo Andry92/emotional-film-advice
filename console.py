d6fcb0f73536b4eea3a80a84073515c608610474
angelo <angelo.megna94@gmail.com>
2019-07-05 17:07:47 +0200

    Fix menù e list out of index Rimane da capire sto bug ""grafico""

--- console.py
+++ console.py
@@ -2,10 +2,10 @@ from Get_Tweets import get_tweets
 from Preprocess_Tweets import preprocess_tweets
 from Topic_process import topic_process
 from cosine_similarity import cosine_similarity_script
-
 from threading import Thread
 
-scelta = input("""
+while True:
+    scelta = input("""
 *******************************************************************
 *                          TOPIC FILM ADVICE                          *
 *                         CONSOLE DI GESTIONE                         *
@@ -20,21 +20,23 @@ scelta = input("""
 *******************************************************************
 READY > """).upper()
 
-
-if scelta=='1':
-    pass
-    t1 = Thread(target=get_tweets())
-    t1.daemon = True
-    t1.start()
-elif scelta=='2':
-    t2 = Thread(target=preprocess_tweets())
-    t2.daemon = True
-    t2.start()
-elif scelta == '3':
-    t3 = Thread(target=topic_process())
-    t3.daemon = True
-    t3.start()
-elif scelta == '4':
-    t4 = Thread(target=cosine_similarity_script())
-    t4.daemon = True
-    t4.start()
+    if scelta == '1':
+        pass
+        t1 = Thread(target=get_tweets())
+        t1.daemon = True
+        t1.start()
+    elif scelta == '2':
+        t2 = Thread(target=preprocess_tweets())
+        t2.daemon = True
+        t2.start()
+    elif scelta == '3':
+        t3 = Thread(target=topic_process())
+        t3.daemon = True
+        t3.start()
+    elif scelta == '4':
+        t4 = Thread(target=cosine_similarity_script())
+        t4.daemon = True
+        t4.start()
+    elif scelta == 'Q':
+        print("FINE")
+        exit(0)
