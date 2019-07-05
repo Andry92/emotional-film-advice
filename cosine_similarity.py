43a63ec19f231aefcb63b0327fae071145e4085c
andrea <andreadibenedetto92@gmail.com>
2019-07-05 11:19:23 +0200

    various fix print cosine_similarity

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -1,24 +1,24 @@
-from pprint import pprint
-
 import numpy
 
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 import csv
 import numpy as np
 
-# Define the documents
 tweet_topic = []
 with open('tweet_topics.csv', 'r+') as tweet_file:
     for tweet in tweet_file.readlines():
         tweet_topic.append(tweet.replace("\n", ""))
 
 movie_topic = []
+movie_title = []
 with open('movie_dataset_very_small.csv', 'r+') as movie_file:
     reader = csv.reader(movie_file, delimiter=';')
     for row in reader:
         movie_topic.append(row[2])
+        movie_title.append(row[1])
+
 
 '''
 tweet_0 = "environment,iceonfir,11,keep,crisi,june,premier,find,colorado,produc"
@@ -36,7 +36,8 @@ movie10 = "american,bare chested male bondage,cia,detention,interrogation"
 '''
 #documents = [tweet_0, movie1, movie2, movie3, movie4, movie5, movie6, movie7, movie8, movie9, movie10]
 documents_def = []
-all_movie_title = []
+all_tweetid_movie_matched = []
+all_tweettopic_movie_matched = []
 
 for i in range(1, len(tweet_topic)+1):
     documents_def = []
@@ -44,9 +45,9 @@ for i in range(1, len(tweet_topic)+1):
     for elem in movie_topic:
         documents_def.append(elem)
 
-    print("VALORE DI i > ", i)
+    #print("VALORE DI i > ", i)
     #print("DOCUMENTS3 > ", documents3)
-    print("DOCUMENTS > ", documents_def)
+    #print("DOCUMENTS > ", documents_def)
 
     count_vectorizer = CountVectorizer()
     sparse_matrix = count_vectorizer.fit_transform(documents_def)
@@ -54,27 +55,26 @@ for i in range(1, len(tweet_topic)+1):
     df = pd.DataFrame(doc_term_matrix, columns=count_vectorizer.get_feature_names())
 
     cos_sim = cosine_similarity(df, df)
-    print("MATRICE COSENO\n", cos_sim)
+    #print("MATRICE COSENO\n", cos_sim)
 
 
     first_column = cos_sim[:, 0]
     sort_first_column = sorted(first_column)
-    print("COLONNA >\n", sort_first_column)
+    print()
+    print("COLONNA (TRASPOSTA) ORDINATA DELLE CORRISPONDENZE TRA IL TWEET {} E LE TRAME > ".format(i), sort_first_column)
     punteggio_max = sort_first_column[-2]
-    print("Punteggio max < di 1: ", punteggio_max)
-    indice_topic_trama = np.where(first_column == sort_first_column[-2])
     if punteggio_max == 0:
-        print("Nessuna corrispondenza\n")
+        print("NESSUNA CORRISPONDENZA TROVATA!")
     else:
-        print("\n\nIndice trama", indice_topic_trama[0])
-        movie_title = []
-        with open('movie_dataset_very_small.csv', 'r+') as movie_file:
-            reader = csv.reader(movie_file, delimiter=';')
-            for row in reader:
-                movie_title.append(row[1])
-        print("Titolo film > ", movie_title[indice_topic_trama[0][0]-1])
-        all_movie_title.append(movie_title[indice_topic_trama[0][0]-1])
-        print("all_movie_title: ", all_movie_title)
+        print("PUNTEGGIO CORRISPONDENZA MASSIMA (esclusa quella per se stesso) > ", punteggio_max)
+        indice_topic_trama = np.where(first_column == sort_first_column[-2])
+        print("INDICE TRAMA PIU' RILEVANTE > ", indice_topic_trama[0])
+        print("TITOLO FILM RILEVANTE > ", movie_title[indice_topic_trama[0][0]-1])
 
+        all_tweetid_movie_matched.append("TWEET ID: " + str(i-1) + " MOVIE TITLE: " + movie_title[indice_topic_trama[0][0]-1])
+        all_tweettopic_movie_matched.append("TWEET TOPIC: " + tweet_topic[i-1] + " MOVIE TITLE: " + movie_title[indice_topic_trama[0][0] - 1])
+
+print("\n\nTWEET ID-FILM MATCHATI: ",all_tweetid_movie_matched ,"\n\n")
+print("TWEET TOPIC-FILM MATCHATI: ",all_tweettopic_movie_matched)
 
 
